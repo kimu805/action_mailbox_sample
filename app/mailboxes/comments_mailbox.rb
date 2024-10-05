87dfe4840691002de60ab751199e29f1b5761b39
kimu805 <kimukimuok805@gmail.com>
2024-10-05 15:35:22 +0900

    Update comments_mailbox.rb

--- app/mailboxes/comments_mailbox.rb
+++ app/mailboxes/comments_mailbox.rb
@@ -1,4 +1,19 @@
 class CommentsMailbox < ApplicationMailbox
+  before_processing :validate_request
   def process
+    board.comments.create!(
+      body: mail.decoded,
+      creator: commenter
+    )
   end
+
+  private
+  def validate_request
+    return if commenter && board
+      bounce_with CommentMailer.invalid_request(
+        inbound_email, commenter: commenter, board: board
+      )
+  end
+
+  
 end
