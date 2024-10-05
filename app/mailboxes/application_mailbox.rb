0897edc56ec6c9e2de6b0c0dd34022b5f64a99a4
kimu805 <kimukimuok805@gmail.com>
2024-10-05 15:29:39 +0900

    Update application_mailbox.rb

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,3 +1,6 @@
 class ApplicationMailbox < ActionMailbox::Base
-  # routing /something/i => :somewhere
+  routing (/[0-9]+-comment@/i) => :comments
+  # routing ->(inbound_email) { inbound_email.mail.to.size > 2 } => :multiple_recipients
+  # routing CustomAddress.new => :custom
+  routing :all => :backstop
 end
